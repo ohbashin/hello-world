f88d4cfa3f0ca2695cca3b62b6f093680da413cd
ohbashin <48339421+ohbashin@users.noreply.github.com>
2024-02-07 16:37:00 +0900

    Update s3Client.js

--- server/utils/s3Client.js
+++ server/utils/s3Client.js
@@ -1,6 +1,20 @@
-import { S3Client } from "@aws-sdk/client-s3";
+//import { S3Client } from "@aws-sdk/client-s3";
 
-const config = useRuntimeConfig();
-const s3Client = new S3Client({ region: config.region });
+//const config = useRuntimeConfig();
+//const s3Client = new S3Client({ region: config.region });
+
+//export default s3Client;
+
+import { S3Client } from "@aws-sdk/client-s3";
+import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
+import { ProxyAgent } from "proxy-agent";
+
+const agent = new ProxyAgent();
+const s3Client = new S3Client({
+  requestHandler: new NodeHttpHandler({
+    httpAgent: agent,
+    httpsAgent: agent,
+  }),
+});
 
 export default s3Client;
